1393fc16eb932b5614bf76b23e7be83f73ae2cf4
senga <ldyjjm@163.com>
2022-09-01 17:00:13 +0800

    vite配置删除lib

--- vite.config.ts
+++ vite.config.ts
@@ -7,11 +7,6 @@ export default defineConfig(({ mode }) => ({
     __APP_VERSION__: JSON.stringify(pkg.version),
   },
   build: {
-    lib: {
-      entry: path.resolve(__dirname, "src/index.tsx"),
-      formats: ["cjs", "es"],
-      fileName: "index",
-    },
     sourcemap: mode === "production",
     outDir: "dist",
     rollupOptions: {
